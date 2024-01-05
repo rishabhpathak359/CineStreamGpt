85a7b028d6012f58bbe7b3bf201feec84d1d7d36
Rishabh <rishabh09p@gmail.com>
2024-01-06 01:19:22 +0530

    ..

--- Netflix/backend/index.mjs
+++ Netflix/backend/index.mjs
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin:["https://netflix-qrtg7oe3j-coderishog.vercel.app/"],
+  origin:["https://netflix-gpt-roan-three.vercel.app/"],
   methods:['POST','GET']
 }));
 
